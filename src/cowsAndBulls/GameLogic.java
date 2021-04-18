38001ad91138d27d69aa93584dbb3a00494e08ce
Vong Beng <vbeng@seek.com.au>
2021-04-18 18:10:38 +1000

    Use hashSet, add some helper functions

--- src/cowsAndBulls/GameLogic.java
+++ src/cowsAndBulls/GameLogic.java
@@ -1,15 +1,17 @@
 package cowsAndBulls;
 
+import java.util.HashSet;
+
 public class GameLogic {
 
 	private String secret;
-	private boolean[] isNumberUsed;
+	private HashSet<Character> numbersUsed;
 	private boolean isSecretCompromised;
 	
 	public GameLogic() {
 		secret = "";
 		isSecretCompromised = false;
-		isNumberUsed = new boolean[10];
+		HashSet<Character> numbersUsed;
 	}
 	
 	public boolean checkIsSecretCompromised() {
@@ -18,13 +20,36 @@ public class GameLogic {
 	
 	public int[] cowsAndBulls(String guess) {
 		int[] cowsAndBulls = new int[2];
+		char digit;
 		
-		for (char digit : guess) {
+		for (int i = 0; i < secret.length(); i++) {
+			digit = secret.charAt(i);
+			
+			// Check if digit is used
+			// Check digit position
+			// Add to cow/bull
 			
 		}
 		
 		return cowsAndBulls;
 	}
+	
+	public boolean setSecret(String number) {
+		if (this.isNumber(number)) {			
+			secret = number.substring(0, 4);
+			numbersUsed.clear();
+			for (char c : secret.toCharArray()) {
+				numbersUsed.add(c);
+			}
+			return true;
+		} else return false;		
+	}
+	
+	private boolean isDigitUsed(char digit) {
+		return numbersUsed.contains(digit);
+	}
+	
+	
 	
 	private boolean isNumber(String string) {
 		if (string == null) return false;
